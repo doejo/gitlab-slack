717902db13879e71764b00e2f897ff4c15a5e0af
Dan Sosedoff <dan.sosedoff@gmail.com>
2014-02-26 20:37:15 -0600

    Refactor

--- lib/message.rb
+++ lib/message.rb
@@ -9,21 +9,8 @@ class Message
   def to_s
     return new_branch_message     if new_branch?
     return deleted_branch_message if deleted_branch?
-
-    message = [
-      payload.user_name,
-      "pushed to branch",
-      "<#{url("/commits/#{head_name}")}|#{head_name}>",
-      "of",
-      "<#{url}|#{head_name}>",
-      "(<#{compare_url}|Compare changes>)"
-    ].join(" ")
-
-    lines = commits.map do |commit|
-      "- #{commit.message.split("\n").first} (<#{commit_url(commit.id)}|#{commit.id[0,6]}>)"
-    end
-
-    message + "\n" + lines.join("\n")
+    
+    push_message + "\n" + commits_lines
   end
 
   private
@@ -42,6 +29,17 @@ class Message
 
   def url(path = nil)
     "#{payload.repository.homepage}#{path}".strip
+  end
+
+  def push_message
+    [
+      payload.user_name,
+      "pushed to branch",
+      "<#{url("/commits/#{head_name}")}|#{head_name}>",
+      "of",
+      "<#{url}|#{head_name}>",
+      "(<#{compare_url}|Compare changes>)"
+    ].join(" ")
   end
 
   def new_branch_message
@@ -70,5 +68,13 @@ class Message
 
   def commit_url(sha)
     url("/commit/#{sha}")
+  end
+
+  def commits_lines
+    lines = commits.map do |commit|
+      "- #{commit.message.split("\n").first} (<#{commit_url(commit.id)}|#{commit.id[0,6]}>)"
+    end
+
+    lines.empty? ? "" : lines.join("\n")
   end
 end
